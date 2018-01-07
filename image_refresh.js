70bc0714fff4fb3db3ddd13bff64898ca536805b
ItsOnlyBinary <github@itsonlybinary.com>
2018-01-07 22:56:08 +0000

    image_refresh.js add znc welcome screen support

--- image_refresh.js
+++ image_refresh.js
@@ -1,28 +1,39 @@
 var refresh_url;
+var target_div;
 
 kiwi.plugin('image_refresh', function() {  
-    var div = document.createElement('div');
-    div.style.position = 'absolute';
-    div.style.top = '5px';
-    div.style.left = '10px'
-    var link = document.createElement('a');
-    link.setAttribute('onclick', 'refresh_img()');
-    link.className = 'u-link';
-    link.style.fontWeight = '900';
-    link.style.fontSize = '2em'
-    link.innerHTML = '&#8634;';
-    div.appendChild(link);
+    var settings = kiwi.state.settings;
+    if (settings.startupScreen == 'welcome') {
+        target_div = document.querySelector('.kiwi-welcome-simple-section-info');
+    }
+    else if (settings.startupScreen == 'znc') {
+        target_div = document.querySelector('.kiwi-welcome-znc-section-info');
+    }
     
-    addLoadEvent(function () {
-        var parent = document.querySelector('.kiwi-welcome-simple-section-info');
-        parent.insertBefore(div, parent.childNodes[0]);
-    });
+    if (target_div != null) {
+        var div = document.createElement('div');
+        div.style.position = 'absolute';
+        div.style.top = '5px';
+        div.style.left = '10px'
+        var link = document.createElement('a');
+        link.setAttribute('onclick', 'refresh_img()');
+        link.className = 'u-link';
+        link.style.fontWeight = '900';
+        link.style.fontSize = '2em'
+        link.innerHTML = '&#8634;';
+        div.appendChild(link);
+        
+        addLoadEvent(function () {
+            target_div.insertBefore(div, target_div.childNodes[0]);
+        });
+    }
 });
 
 function refresh_img() {
-    var parent = document.querySelector(".kiwi-welcome-simple-section-info");
     if (refresh_url == null) {
-        refresh_url = parent.style.backgroundImage;
+        refresh_url = target_div.style.backgroundImage;
     }
-    parent.style.backgroundImage = refresh_url.replace(/\?/i, '?ctime=' + new Date().getTime() + '&');
+    if (refresh_url != null) {
+        target_div.style.backgroundImage = refresh_url.replace(/\?/i, '?ctime=' + Math.floor(new Date().getTime() / 1000) + '&');
+    }
 }
